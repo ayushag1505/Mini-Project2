516008302355d83c698d50e9971ceaec1e98b40f
Ayush Agrawal <111371722+ayush360@users.noreply.github.com>
2024-02-25 00:01:15 +0530

    Project Setup

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express') ;
+const app = express() ;
+const path = require('path') ;
+const mongoose = require('mongoose') ;
+const methodOverride = require('method-override') ;
+
+app.set('view engine', 'ejs') ;
+app.set('views', path.join(__dirname, 'views')) ;
+
+app.use(express.static(path.join(__dirname, 'public'))) ;
+app.use(express.urlencoded({extended: true})) ;
+app.use(methodOverride('_method')) ;
+
+mongoose.connect('mongodb://127.0.0.1:27017/recipe')
+.then(()=>{
+    console.log('Database Connected');
+})
+.catch((err)=>{
+    console.log(err);
+})
+
+app.get('/', (req, res)=>{
+    res.send('Hello') ;
+})
+
+app.listen(3000, ()=>{
+    console.log('Server Connected at 3000');
+})
